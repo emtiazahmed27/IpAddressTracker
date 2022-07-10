bc1828a614ba122a6a59bc30b94e75094021f507
emtiazahmed27 <emtihstu127@gmail.com>
2022-07-10 20:31:35 +0600

    last commit

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ const input=document.querySelector("input"),
 //function that collects IP Address
 function getIp(){
     if(input.value===""){
-        swal("fetching default device location")
+        Swal.fire({
+            title:'Fetching Default Device Location',
+            showConfirmButton:false,
+            timer:1500,
+        })
         return input.value;
     }
     else return input.value;
@@ -22,7 +26,7 @@ btn.addEventListener("click",(e)=>{
     const ip=getIp();
     console.log(ip);
 
-    let api=`http://ip-api.com/json/${ip}`
+    let api=`https://geo.ipify.org/api/v2/country,city?apiKey=at_pqYDBkhOgn2nnBRTJKsoFTE56IlLd&ipAddress=${ip}`
     function getLocation(){
         //fetching data
         fetch(api)
@@ -30,9 +34,13 @@ btn.addEventListener("click",(e)=>{
                 console.log(data);
                 
                 //error handling
-                if (data.status==="fail"){
+                if (data.code===422){
                     //calling swal() if any error occurs 
-                    swal("invalid query");
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Ivalid Query',
+                      })
                     //setting output fields to its initial value
                     loc.innerText="__";
                     timeZone.innerText="__";
@@ -40,10 +48,10 @@ btn.addEventListener("click",(e)=>{
                     ipAdd.innerText="__";
                 }
                 else{
-                    loc.innerText=`${data.city},${data.country}`;
-                    timeZone.innerText=data.timezone;
+                    loc.innerText=`${data.location.region},${data.location.country}`;
+                    timeZone.innerText=data.location.timezone;
                     isp.innerText=data.isp;
-                    ipAdd.innerText=data.query;
+                    ipAdd.innerText=data.ip;
                 }
             })  
     }
